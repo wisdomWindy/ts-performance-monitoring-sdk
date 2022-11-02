9cfa136c149d4ddfe43747d88ad35f9521b23c42
淼 <oncwnuAY3AEycYlfemOQQ1CvcijQ@git.weixin.qq.com>
2022-11-02 16:09:17 +0800

    添加html文件

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('index.ts')
+console.log('index.ts');
+interface Info {
+  name:string;
+  age:number;
+}
+type K = number;
+
+// type I = 'x' | 'y';
+// let g: Record<I, K> = {
+//   x: 1,
+//   y: 2
+// }
+
+
+type U = keyof Info;
+
+let f: Record<U, K> = {
+  name:1,
+  age:10
+}
